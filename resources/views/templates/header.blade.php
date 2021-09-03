37af6a92d5c1b599927ed703d377a086cc60d0b1
vvalcarcell <valcarcellvalentina@gmail.com>
2021-09-03 17:57:41 +0200

    bonus (manca css)

--- resources/views/templates/header.blade.php
+++ resources/views/templates/header.blade.php
@@ -1,12 +1,16 @@
 <div class="container">
     <img src="/img/dc-logo.png" alt="logo" />
     <ul>
-      <li><a href="#">Characters</a></li>
+      <li >
+          <a href="#">Characters</a>
+      </li>
       <li><a href="#">Comics</a></li>
       <li><a href="#">Movies</a></li>
       <li><a href="#">Tv</a></li>
       <li><a href="#">Games</a></li>
-      <li><a href="#">Collectibles</a></li>
+      <li class="{{ Request::route()->getName() === 'home' ? 'active' : '' }}">
+          <a href="{{ route('home') }}">Collection</a>
+      </li>
       <li><a href="#">Videos</a></li>
       <li><a href="#">Fans</a></li>
       <li><a href="#">News</a></li>
