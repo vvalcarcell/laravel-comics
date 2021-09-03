a01b37f8cb264c5f1ac9459a8e5eca6b671c2295
vvalcarcell <valcarcellvalentina@gmail.com>
2021-09-03 22:13:38 +0200

    route2 completa

--- resources/views/singleComic.blade.php
+++ resources/views/singleComic.blade.php
@@ -6,17 +6,18 @@
 @section('content')
     <div class="single-Comic">
         <div class="jumbo"></div>
-        <div class="blu-ribbon">
+        <section class="blu-ribbon">
             <div class="container">
                 <img src="{{$comic['thumb']}}" alt="">
             </div>
-        </div>
+        </section>
 
-        <div class="container-comic">
-            <div class="right-item">
+        <section class="container-comic">
+
+            <div class="left-item">
                 <h1>{{$comic['title']}}</h1>
-                <div class="green-ribbon">
-                    <div class="item-left">
+                <div class="green-ribbon flex">
+                    <div class="item-left flex">
                         <div class="price-info">
                             <span class="price">U.S. Price:</span>
                             <span class="bold">{{$comic['price']}}</span>
@@ -27,20 +28,70 @@
                         <div class="bold">Check Availability</div>
                     </div>
                 </div>
-                <p>
-                    Lorem ipsum dolor sit amet, consectetur adipisicing elit. Praesentium quisquam doloremque deserunt enim, consequatur ipsam animi quaerat quam repellat harum rerum quae qui ea reprehenderit atque dolore obcaecati, sunt nostrum.
-                    Lorem ipsum dolor sit amet consectetur, adipisicing elit. Doloribus nemo eligendi, reprehenderit id vitae maiores aut deserunt tempore vel sequi molestiae tempora laboriosam officia rerum optio veritatis consequatur? Id, debitis!
-                </p>
+                <p>{{ $comic['description']}}</p>
             </div>
-            <div class="left-item">
-                <div class="adv">advertisement</div>
-                <div class="adv-box">
+
+            <div class="right-item">
+                <div class="adv flex">advertisement</div>
+                <div class="adv-box flex">
                     <img src="/img/adv.jpg" alt="">
                 </div>
-            </div>
-            
+            </div>          
           
-        </div>
+        </section>
+
+        <section class="grey-section">
+            <div class="container flex">
+                <div class="column left">
+                    <h2>Talent</h2>
+
+                    <div class="info-by flex">
+                        <div class="bold">Art by:</div>
+                        <p>
+                            @foreach($comic['artists'] as $artist)
+                                {{ $artist }},
+
+                            @endforeach
+                        </p>
+                    </div>
+                    <div class="info-by flex">
+                        <div class="bold">Written by:</div>
+                        <p>
+                            @foreach($comic['writers'] as $writer)
+                                {{ $writer }},
+
+                            @endforeach
+                        </p>
+                    </div>
+
+                </div>
+                <div class="column right">
+                    <h2>Specs</h2>
+
+                    <div class="info-by flex">
+                        <div class="bold">Series:</div>
+                        <p class="uppercase">
+                            {{ $comic['series'] }}
+                        </p>
+                    </div>
+                    <div class="info-by flex">
+                        <div class="bold">U.S. Price:</div>
+                        <p>
+                             {{ $comic['price'] }}
+                        </p>
+                    </div>
+                    <div class="info-by flex">
+                        <div class="bold">On Sale Date:</div>
+                        <p>
+                            {{ $comic['sale_date'] }}
+                        </p>
+                    </div>
+                </div>
+            </div>
+        
+        </section>
+
+
 
     </div>
 
